915419715624a8bf533ce23105486d0081196314
unknown <ajaymkumar172@gmail.com>
2023-07-18 18:05:35 +0530

    final update

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use('/api/upload',UploadRouter);
 
 app.get('/api/config/paypal',(req,res)=>res.json({clientId:process.env.PAYPAL_CLIENT_ID}))
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODEENV === 'production') {
     const __dirname = path.resolve();
     app.use('/uploads', express.static('/var/data/uploads'));
     app.use(express.static(path.join(__dirname, '/frontend/build')));

6ff6f857ab474b19a4c404546100cf006c1be323
unknown <ajaymkumar172@gmail.com>
2023-07-19 12:16:55 +0530

    env variable

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,13 @@ if (process.env.NODEENV === 'production') {
       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     );
   }
-
+   else {
+     const __dirname = path.resolve();
+   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+     app.get('/', (req, res) => {
+       res.send('API is running....');
+     });
+   }
 
 app.use(notFound);
 app.use(errorHandler)
